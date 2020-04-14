257410531b7faff9fa23c4b06c795fc2593bd0ff
Michael Elnajami <makmoud98@gmail.com>
2020-04-13 23:34:15 -0400

    created basic routes

--- app.js
+++ app.js
@@ -3,5 +3,8 @@ const app = express()
 const port = 8080
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/accounts/', require('./routes/accounts.js'))
+app.get('/rides/', require('./routes/rides.js'))
+app.get('/reports/', require('./routes/reports.js'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
